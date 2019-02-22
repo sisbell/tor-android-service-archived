f3b3df4e66630a68c9987d52d93580549a0acaf4
sisbell <shane.isbell@gmail.com>
2019-02-22 09:00:25 -0800

    Fixes #6: Change Constant for Browser App

--- service/src/main/java/org/torproject/android/service/TorServiceConstants.java
+++ service/src/main/java/org/torproject/android/service/TorServiceConstants.java
@@ -9,7 +9,7 @@ import android.content.Intent;
 public interface TorServiceConstants {
 
     String TOR_APP_USERNAME = "org.torproject.android";
-    String BROWSER_APP_USERNAME = "info.guardianproject.orfox";
+    String BROWSER_APP_USERNAME = "org.torproject.torbrowser";
 
     String DIRECTORY_TOR_BINARY = "bin";
     String SHELL_CMD_PS = "toolbox ps";
